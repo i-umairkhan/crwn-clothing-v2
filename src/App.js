24bbb1a78bb23375d18849395c09aead32acd4ca
umair2607 <umair.k2607@gmail.com>
2022-08-22 07:47:08 +0500

    added navigation , shop component react router for browser routing.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
-const App = () => {
-  return <Home />;
+import React from "react";
+
+const Shop = () => {
+  return <h1>Shop</h1>;
 };
 
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
 export default App;
